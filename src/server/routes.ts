3ffdb175905fae50dba72eb1334e2bf130f2c340
Elvis Hernandez <elvishernandezdev@gmail.com>
2024-11-09 21:05:43 -0500

    Return RAF data to client in the /api/patients endpoint

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,11 +1,41 @@
 import { Router } from "express";
-import { getPatients } from "./database/helpers";
+import { getRiskProfilesByPatientId, getPatients, getRiskProfilesBySegmentName, getHighestSegmentNameRAFScore, getPatientRAFScore } from "./database/helpers";
+import { PatientsApiResponse } from "./types";
 
 const router = Router();
 
-router.get("/api/patients", async (req, res) => {
-  const patients = await getPatients();
-  return res.json(patients);
+router.get("/api/patients", async (_, res) => {
+  let statusCode = 200;
+  let responseBody: PatientsApiResponse = {};
+
+  try {
+    const [
+      patientsWithoutRAFScore,
+      riskProfilesByPatientId,
+      riskProfilesBySegmentName,
+    ] = await Promise.all([
+      getPatients(),
+      getRiskProfilesByPatientId(),
+      getRiskProfilesBySegmentName(),
+    ]);
+
+    const patientsWithRAFScore = patientsWithoutRAFScore.map(
+      (patient) => getPatientRAFScore(patient, riskProfilesByPatientId)
+    );
+    const [segmentName, rafScore] = getHighestSegmentNameRAFScore(riskProfilesBySegmentName);
+
+    responseBody = {
+      patients: patientsWithRAFScore,
+      segmentNameWithHighestRafScore: segmentName,
+      highestSegmentNameRafScore: rafScore,
+    };
+  }
+  catch (e) {
+    statusCode = 500;
+    console.error(e);
+  }
+
+  return res.status(statusCode).json(responseBody);
 });
 
 export { router }

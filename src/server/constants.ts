484f385567168a8c09b76977067c3a1f8e826c15
Elvis Hernandez <elvishernandezdev@gmail.com>
2024-11-09 21:07:17 -0500

    Add /api/patients POST endpoint for adding new patients

---
+++ src/server/constants.ts
@@ -0,0 +1,9 @@
+import { EnrollmentStatus } from "./types";
+
+export const enrollmentStatuses: Array<EnrollmentStatus> = [
+  "Prospect",
+  "Insurance Eligibility Verified",
+  "Enrolled Contract Signed",
+  "Patient Record Created",
+  "Intake Appointment Scheduled",
+];
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { enrollmentStatuses } from "./constants";
+import { patients } from "./database/fakeDatabaseData";
 import { getRiskProfilesByPatientId, getPatients, getRiskProfilesBySegmentName, getHighestSegmentNameRAFScore, getPatientRAFScore } from "./database/helpers";
-import { PatientsApiResponse } from "./types";
+import { EnrollmentStatus, Patient, PatientsApiResponse } from "./types";
 
 const router = Router();
 
@@ -36,6 +38,46 @@ router.get("/api/patients", async (_, res) => {
   }
 
   return res.status(statusCode).json(responseBody);
+});
+
+router.post("/api/patients", async (req, res) => {
+  const { name, enrollmentStatus } = req.body;
+
+  const response: {
+    errors: string[],
+    body: Patient | null,
+  } = {
+    errors: [],
+    body: null,
+  }
+  let statusCode = 200;
+
+  if (!name || typeof name !== 'string') {
+    response.errors.push('Name is a required string parameter');
+    statusCode = 400;
+  }
+
+  if (
+    !enrollmentStatus ||
+    typeof enrollmentStatus !== 'string' ||
+    !enrollmentStatuses.includes(enrollmentStatus as EnrollmentStatus)
+  ) {
+    response.errors.push('Enrollment status is a required parameter and must be one of the valid types');
+    statusCode = 400;
+  }
+
+  if (statusCode === 200) {
+    const lastPatientId = patients[patients.length - 1]?.id || 999;
+    const newPatient = {
+      id: lastPatientId + 1,
+      name,
+      enrollmentStatus,
+    }
+    patients.push(newPatient);
+    response.body = newPatient;
+  }
+
+  return res.status(statusCode).json(response);
 });
 
 export { router }

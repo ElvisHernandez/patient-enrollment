bf4ce1ffe60ca474e4f82be772c8e8d2500cb169
Elvis Hernandez <elvishernandezdev@gmail.com>
2024-11-09 20:52:41 -0500

    Add a shared Layout component

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,13 +4,24 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { Patients } from "./routes/patients";
 
-import { Root } from "./routes/root";
+import './index.scss';
+import { Layout } from "./layout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Root />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Patients />,
+      },
+      {
+        path: "/patients",
+        element: <Patients />,
+      },
+    ],
   },
 ]);
 
